b2e858c409303647afc22218c7cc1e599a94629c
pythonjedi <myseoru@gmail.com>
2020-04-20 00:48:06 +0300

    battle alpha

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Thing:
 
     def __init__(self, defence=0, attack=0, hp=0):
         while self.count < 5:
-            self.defence = defence or random.randint(1, 50)
+            self.defence = defence or random.randint(1, 5)/100
             self.attack = attack or random.randint(1, 50)
             self.hp = hp or random.randint(50, 100)
             self.count += 1
@@ -36,17 +36,23 @@ class Person:
             self.attack += thing.attack
             self.defence += thing.defence
 
+    def minus_hp(self):
+        pass
+
+
 class Paladin(Person):
     def __init__(self, hp, name, attack, defence):
         super().__init__(hp, name, attack, defence)
         self.hp *= 2
         self.defence *= 2
+        self.class_name = 'Paladin'
 
 
 class Warrior(Person):
     def __init__(self, hp, name, attack, defence):
         super().__init__(hp, name, attack, defence)
         self.attack *= 2
+        self.class_name = 'Warrior'
 
 classes = [Paladin, Warrior] #классы
 rand = random.randint(1, 10) #начальные характеристики для персонажа
@@ -62,9 +68,21 @@ hero_9 = random.choice(classes)(rand, random.choice(list_nanes), rand, rand)
 hero_10 = random.choice(classes)(rand, random.choice(list_nanes), rand, rand)
 hero_1.setThings(things)
 hero_2.setThings(things)
+hero_3.setThings(things)
+hero_4.setThings(things)
+hero_5.setThings(things)
+hero_6.setThings(things)
+hero_7.setThings(things)
+hero_8.setThings(things)
+hero_9.setThings(things)
+hero_10.setThings(things)
+
 
 
 print('======================================')
-print(f'Сегодня у нас встречается непобедимый {hero_1.name} с атакой {hero_1.attack}, защитой {hero_1.defence}\n и непорившийся {hero_2.name} с  атакой {hero_2.attack}, защитой {hero_2.defence}!')
+print(f'На арену вышли:\n{hero_1.name}, класса {hero_1.class_name}\n{hero_2.name}, класса {hero_2.class_name}\n{hero_3.name}, класса {hero_3.class_name}\n{hero_4.name}, класса {hero_4.class_name}\n{hero_5.name}, класса {hero_5.class_name}\n{hero_6.name}, класса {hero_6.class_name}\n{hero_7.name}, класса {hero_7.class_name}\n{hero_8.name}, класса {hero_8.class_name}\n{hero_9.name}, класса {hero_9.class_name}\n{hero_10.name}, класса {hero_10.class_name}')
 print('======================================')
 
+def battle():
+    attack_hero = hero_1
+    def_hero = hero_3
